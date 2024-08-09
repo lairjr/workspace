551219471b5f48992d938f54960ce822c0a2360a
Lair Junior <lairjr@outlook.com>
2024-08-09 13:57:10 +0000

    Remove data

--- test/go_champs_scoreboard/games/games_test.exs
+++ test/go_champs_scoreboard/games/games_test.exs
@@ -4,23 +4,38 @@ defmodule GoChampsScoreboard.Games.GamesTest do
   alias GoChampsScoreboard.Games.Models.TeamState
   alias GoChampsScoreboard.Games.Models.GameState
 
-  describe "find_or_bootstrap/1 when game is database" do
-    setup :set_test_game
-
+  describe "find_or_bootstrap/1 when game is set" do
     test "returns game_state" do
+      set_test_game()
+
       result_game_state = Games.find_or_bootstrap("some-game-id")
 
       assert result_game_state.id == "some-game-id"
       assert result_game_state.away_team.name == "Some away team"
       assert result_game_state.home_team.name == "Some home team"
+
+      unset_test_game()
     end
   end
 
-  defp set_test_game(_context) do
+  describe "find_or_bootstrap/1 when game is not set" do
+    test "bootstraps game and returns it" do
+      result_game_state = Games.find_or_bootstrap("some-game-id")
+
+      assert result_game_state.id == "some-game-id"
+      assert result_game_state.away_team.name == "Away team"
+      assert result_game_state.home_team.name == "Home team"
+    end
+  end
+
+  defp set_test_game() do
     away_team = TeamState.new("Some away team")
     home_team = TeamState.new("Some home team")
     game_state = GameState.new("some-game-id", away_team, home_team)
     Redix.command(:games_cache, ["SET", "some-game-id", game_state])
-    :ok
+  end
+
+  defp unset_test_game() do
+    Redix.command(:games_cache, ["DEL", "some-game-id"])
   end
 end
