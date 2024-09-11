b3e8f5e3152fb7a9a89d22165c49f8b43df3fee9
Lair Junior <lairjr@outlook.com>
2024-09-11 00:42:36 +0000

    Create player stats values

--- lib/go_champs_scoreboard/games/models/player_state.ex
+++ lib/go_champs_scoreboard/games/models/player_state.ex
@@ -2,14 +2,16 @@ defmodule GoChampsScoreboard.Games.Models.PlayerState do
   @type t :: %__MODULE__{
     id: String.t(),
     name: String.t(),
+    stats_values: map()
   }
-  defstruct [:id, :name]
+  defstruct [:id, :name, :stats_values]
 
-  @spec new(String.t(), String.t()) :: t()
-  def new(id, name) do
+  @spec new(String.t(), String.t(), map()) :: t()
+  def new(id, name, stats_values \\ %{}) do
     %__MODULE__{
       id: id,
-      name: name
+      name: name,
+      stats_values: stats_values
     }
   end
 end
--- lib/go_champs_scoreboard/games/models/team_state.ex
+++ lib/go_champs_scoreboard/games/models/team_state.ex
@@ -4,16 +4,18 @@ defmodule GoChampsScoreboard.Games.Models.TeamState do
   @type t :: %__MODULE__{
     name: String.t(),
     score: integer(),
-    players: list(PlayerState.t())
+    players: list(PlayerState.t()),
+    total_player_stats: map()
   }
-  defstruct [:name, :score, :players]
+  defstruct [:name, :score, :players, :total_player_stats]
 
-  @spec new(String.t(), integer(), list(PlayerState.t())) :: t()
-  def new(name, score \\ 0, players \\ []) do
+  @spec new(String.t(), integer(), list(PlayerState.t()), map()) :: t()
+  def new(name, score \\ 0, players \\ [], total_player_stats \\ %{}) do
     %__MODULE__{
       name: name,
       score: score,
       players: players,
+      total_player_stats: total_player_stats,
     }
   end
 end
