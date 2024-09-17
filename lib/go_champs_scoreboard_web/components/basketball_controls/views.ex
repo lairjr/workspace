d16438c3208ec901fb1aded64f5f112aed478880
Lair Junior <lairjr@outlook.com>
2024-09-17 03:34:17 +0000

    Add ability to select player

--- lib/go_champs_scoreboard_web/components/basketball_controls/views.ex
+++ lib/go_champs_scoreboard_web/components/basketball_controls/views.ex
@@ -6,46 +6,46 @@ defmodule Components.BasketballControls.Views do
 
   def general(assigns) do
     ~H"""
-      <div class="container">
-        <.teams_header game_state={@game_state} />
-        <Components.BasketballControls.PlayerStats.view />
-      </div>
+    <div class="container">
+      <.teams_header game_state={@game_state} />
+      <Components.BasketballControls.PlayerStats.view game_state={@game_state} />
+    </div>
     """
   end
 
   def teams_header(assigns) do
     ~H"""
-      <div class="columns basketball-teams-header">
-        <div class="column home-team">
-          <div class="columns">
-            <div class="column has-text-right">
-              <h3 class="title is-3">
-                <%= @game_state.home_team.name %>
-              </h3>
-            </div>
-            <div class="column is-2 has-text-right">
-              <h3 class="title is-3">
-                <%= @game_state.home_team.score %>
-              </h3>
-            </div>
+    <div class="columns basketball-teams-header">
+      <div class="column home-team">
+        <div class="columns">
+          <div class="column has-text-right">
+            <h3 class="title is-3">
+              <%= @game_state.home_team.name %>
+            </h3>
           </div>
-        </div>
-
-        <div class="column away-team">
-          <div class="columns">
-            <div class="column is-2">
-              <h3 class="title is-3">
-                <%= @game_state.away_team.score %>
-              </h3>
-            </div>
-            <div class="column">
-              <h3 class="title is-3">
-                <%= @game_state.away_team.name %>
-              </h3>
-            </div>
+          <div class="column is-2 has-text-right">
+            <h3 class="title is-3">
+              <%= @game_state.home_team.score %>
+            </h3>
           </div>
         </div>
       </div>
+
+      <div class="column away-team">
+        <div class="columns">
+          <div class="column is-2">
+            <h3 class="title is-3">
+              <%= @game_state.away_team.score %>
+            </h3>
+          </div>
+          <div class="column">
+            <h3 class="title is-3">
+              <%= @game_state.away_team.name %>
+            </h3>
+          </div>
+        </div>
+      </div>
+    </div>
     """
   end
 end
