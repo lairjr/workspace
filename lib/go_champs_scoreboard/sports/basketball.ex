c21c7b858df14ad34415342468ee6ee5b0291a07
Lair Junior <lairjr@outlook.com>
2024-09-18 02:59:02 +0000

    Rename fauls to fouls

--- lib/go_champs_scoreboard/sports/basketball.ex
+++ lib/go_champs_scoreboard/sports/basketball.ex
@@ -3,9 +3,8 @@ defmodule GoChampsScoreboard.Sports.Basketball do
     %{key: "points"},
     %{key: "rebounds"},
     %{key: "assists"},
-    %{key: "faults"}
+    %{key: "fouls"}
   ]
-
   def bootstrap() do
     Enum.reduce(@player_stats, %{}, fn stat, player_stats ->
       Map.merge(player_stats, %{[stat.key] => 0})
