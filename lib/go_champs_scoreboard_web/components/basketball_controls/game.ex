c240b558e5df90a10f82dc7ad6a0d393cfc4e100
Lair Junior <lairjr@outlook.com>
2024-09-30 13:29:57 +0000

    Add game controls

---
+++ lib/go_champs_scoreboard_web/components/basketball_controls/game.ex
@@ -0,0 +1,118 @@
+defmodule Components.BasketballControls.Game do
+  use Phoenix.Component
+
+  def time_controls(assigns) do
+    ~H"""
+      <div class="columns is-multiline">
+        <div class="column is-12 has-text-centered">
+          <button class="button">
+            <%= "<" %>
+          </button>
+
+          <button class="button">
+            2o
+          </button>
+
+          <button class="button">
+            <%= ">" %>
+          </button>
+        </div>
+
+        <div class="column is-12 has-text-centered">
+          <button class="button">
+            <%= "<" %>
+          </button>
+
+          <button class="button">
+            09:21:32
+          </button>
+
+          <button class="button">
+            <%= ">" %>
+          </button>
+        </div>
+
+        <div class="column is-12 has-text-centered">
+          <button class="button">
+            Start
+          </button>
+
+          <button class="button">
+            Pause
+          </button>
+        </div>
+      </div>
+    """
+  end
+
+  def home_team_totals(assigns) do
+    ~H"""
+    <div class="columns is-multiline">
+      <div class="column is-12">
+        <button class="button">
+          Active log
+        </button>
+
+        <button class="button">
+          Undo
+        </button>
+
+        <button class="button">
+          T. Foul
+        </button>
+      </div>
+
+      <div class="column is-12 has-text-right">
+        <button class="button is-large">
+          <%= @game_state.home_team.name %>
+        </button>
+
+        <button class="button is-large">
+          <%= @game_state.home_team.score %>
+        </button>
+      </div>
+    </div>
+    """
+  end
+
+  def away_team_totals(assigns) do
+    ~H"""
+    <div class="columns is-multiline">
+      <div class="column is-12">
+      </div>
+
+      <div class="column is-12">
+        <button class="button is-large">
+          <%= @game_state.away_team.score %>
+        </button>
+
+        <button class="button is-large">
+          <%= @game_state.away_team.name %>
+        </button>
+      </div>
+    </div>
+    """
+  end
+
+  def main(assigns) do
+    ~H"""
+    <div class="columns is-multiline">
+      <div class="column is-12">
+        <div class="columns">
+          <div class="column is-4">
+            <Components.BasketballControls.Game.home_team_totals game_state={@game_state} />
+          </div>
+
+          <div class="column is-4 has-text-centered">
+            <Components.BasketballControls.Game.time_controls />
+          </div>
+
+          <div class="column is-4">
+            <Components.BasketballControls.Game.away_team_totals game_state={@game_state} />
+          </div>
+        </div>
+      </div>
+    </div>
+    """
+  end
+end
--- lib/go_champs_scoreboard_web/components/basketball_controls/views.ex
+++ lib/go_champs_scoreboard_web/components/basketball_controls/views.ex
@@ -7,47 +7,12 @@ defmodule Components.BasketballControls.Views do
   def general(assigns) do
     ~H"""
     <div class="container">
-      <.teams_header game_state={@game_state} />
+      <Components.BasketballControls.Game.main
+        game_state={@game_state} />
       <Components.BasketballControls.PlayerStats.view
         game_state={@game_state}
         selected_player={@selected_player}
       />
-    </div>
-    """
-  end
-
-  def teams_header(assigns) do
-    ~H"""
-    <div class="columns basketball-teams-header">
-      <div class="column home-team">
-        <div class="columns">
-          <div class="column has-text-right">
-            <h3 class="title is-3">
-              <%= @game_state.home_team.name %>
-            </h3>
-          </div>
-          <div class="column is-2 has-text-right">
-            <h3 class="title is-3">
-              <%= @game_state.home_team.score %>
-            </h3>
-          </div>
-        </div>
-      </div>
-
-      <div class="column away-team">
-        <div class="columns">
-          <div class="column is-2">
-            <h3 class="title is-3">
-              <%= @game_state.away_team.score %>
-            </h3>
-          </div>
-          <div class="column">
-            <h3 class="title is-3">
-              <%= @game_state.away_team.name %>
-            </h3>
-          </div>
-        </div>
-      </div>
     </div>
     """
   end
