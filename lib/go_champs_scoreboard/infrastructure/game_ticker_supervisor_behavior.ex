ce471d69be9e8228256b0bc58c97f2eab3303d80
Lair Junior <lairjr@outlook.com>
2024-10-29 23:37:50 +0000

    Test lint job

--- lib/go_champs_scoreboard/infrastructure/game_ticker_supervisor_behavior.ex
+++ lib/go_champs_scoreboard/infrastructure/game_ticker_supervisor_behavior.ex
@@ -1,5 +1,5 @@
 defmodule GoChampsScoreboard.Infrastructure.GameTickerSupervisorBehavior do
   @callback get_game_ticker(String.t()) :: :ok | {:error, any()}
-  @callback start_game_ticker(String.t()) :: :ok | {:error, any()}
+    @callback start_game_ticker(String.t()) :: :ok | {:error, any()}
   @callback stop_game_ticker(String.t()) :: :ok | {:error, any()}
 end
