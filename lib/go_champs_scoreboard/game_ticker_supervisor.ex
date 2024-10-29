d2b8733b1a2957291fc367cf59e762f5dfad1c40
Lair Junior <lairjr@outlook.com>
2024-10-29 23:20:21 +0000

    Specify TTL for game tickers

--- lib/go_champs_scoreboard/game_ticker_supervisor.ex
+++ lib/go_champs_scoreboard/game_ticker_supervisor.ex
@@ -3,6 +3,8 @@ defmodule GoChampsScoreboard.GameTickerSupervisor do
 
   @behaviour GoChampsScoreboard.GameTickerSupervisorBehavior
 
+  @two_days_in_milliseconds 172_800_000
+
   def start_link(_arg) do
     DynamicSupervisor.start_link(__MODULE__, :ok, name: __MODULE__)
   end
@@ -14,7 +16,13 @@ defmodule GoChampsScoreboard.GameTickerSupervisor do
 
   @impl true
   def start_game_ticker(game_id) do
-    child_spec = {GoChampsScoreboard.GameTicker, game_id}
+    child_spec = %{
+      id: game_id,
+      start: {GoChampsScoreboard.GameTicker, :start_link, [game_id]},
+      type: :worker,
+      restart: :transient,
+      shutdown: @two_days_in_milliseconds
+    }
     DynamicSupervisor.start_child(__MODULE__, child_spec)
   end
 
