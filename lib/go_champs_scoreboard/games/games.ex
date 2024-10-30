53a8eb31bca55e6b568cf7acf6e17aed0385da73
Lair Junior <lairjr@outlook.com>
2024-10-30 12:52:12 +0000

    Refactor to only use get_game in handle event

--- lib/go_champs_scoreboard/games/games.ex
+++ lib/go_champs_scoreboard/games/games.ex
@@ -8,31 +8,34 @@ defmodule GoChampsScoreboard.Games.Games do
 
   @spec find_or_bootstrap(String.t()) :: GameState.t()
   def find_or_bootstrap(game_id) do
-    case Redix.command(:games_cache, ["GET", game_id]) do
+    case get_game(game_id) do
       {:ok, nil} ->
         game_state =
           Bootstrapper.bootstrap()
           |> Bootstrapper.bootstrap_from_go_champs(game_id)
 
-        Redix.command(:games_cache, ["SET", game_id, game_state])
+        update_game(game_state)
 
-        game_state
-
-      {:ok, curr_game_json} ->
-        GameState.from_json(curr_game_json)
+      {:ok, game} ->
+        game
     end
   end
 
   @spec handle_event(String.t(), String.t(), any()) :: GameState.t()
   def handle_event(game_id, event, event_payload \\ %{}) do
-    current_game_state = find_or_bootstrap(game_id)
-    new_game_state = EventHandlers.handle(event, current_game_state, event_payload)
-    Redix.command(:games_cache, ["SET", game_id, new_game_state])
+    case get_game(game_id) do
+      {:ok, nil} ->
+        raise RuntimeError, message: "Game not found"
 
-    PubSub.broadcast_game_update(game_id, new_game_state)
-    Producers.publish_game_event(new_game_state)
+      {:ok, current_game_state} ->
+        new_game_state = EventHandlers.handle(event, current_game_state, event_payload)
+        update_game(new_game_state)
 
-    new_game_state
+        PubSub.broadcast_game_update(game_id, new_game_state)
+        Producers.publish_game_event(new_game_state)
+
+        new_game_state
+    end
   end
 
   @spec update_team(GameState.t(), String.t(), TeamState.t()) :: GameState.t()
@@ -52,5 +55,25 @@ defmodule GoChampsScoreboard.Games.Games do
   @spec update_clock_state(GameState.t(), GameClockState.t()) :: GameState.t()
   def update_clock_state(game_state, clock_state) do
     %{game_state | clock_state: clock_state}
+  end
+
+  @spec get_game(String.t()) :: {:ok, GameState.t()} | {:ok, nil} | {:error, any()}
+  defp get_game(game_id) do
+    case Redix.command(:games_cache, ["GET", game_id]) do
+      {:ok, nil} ->
+        {:ok, nil}
+
+      {:ok, game_json} ->
+        {:ok, GameState.from_json(game_json)}
+
+      {:error, error} ->
+        {:error, error}
+    end
+  end
+
+  @spec update_game(GameState.t()) :: GameState.t()
+  defp update_game(game_state) do
+    Redix.command(:games_cache, ["SET", game_state.id, game_state])
+    game_state
   end
 end
