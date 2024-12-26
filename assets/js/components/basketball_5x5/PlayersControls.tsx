f9e5ea365806117f13c4b484b69d48ebd0d77d0c
Lair Junior <lairjr@outlook.com>
2024-12-26 12:57:14 +0000

    Simplify substitue experience

--- assets/js/components/basketball_5x5/PlayersControls.tsx
+++ assets/js/components/basketball_5x5/PlayersControls.tsx
@@ -17,39 +17,57 @@ function PlayersControls({
   teamType,
   selectPlayer,
 }: PlayersControlsProps) {
-  const [showSubstituteModal, setShowSubstituteModal] = React.useState(false);
+  const [showSubstitutePlayers, setShowSubstitutePlayers] =
+    React.useState(false);
   const playingPlayers = team.players.slice(0, 5);
+  const benchPlayers = team.players.slice(5);
+  const onSubstitute = (playerId: string) => {
+    setShowSubstitutePlayers(false);
+    console.log('substitute', playerId);
+  };
 
   return (
     <div className="columns is-multiline">
-      <ul className="column is-12">
-        {playingPlayers.map((player) => (
-          <li key={player.id}>
-            <button
-              className="button is-dark is-outlined is-fullwidth"
-              onClick={() =>
-                selectPlayer({ playerId: player.id, teamType: teamType })
-              }
-            >
-              {player.name + ' - ' + player.number}
-            </button>
-          </li>
-        ))}
-      </ul>
+      {!showSubstitutePlayers && (
+        <ul className="column is-12">
+          {playingPlayers.map((player) => (
+            <li key={player.id}>
+              <button
+                className="button is-dark is-outlined is-fullwidth test-class"
+                onClick={() =>
+                  selectPlayer({ playerId: player.id, teamType: teamType })
+                }
+              >
+                {player.name + ' - ' + player.number}
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
+
+      {showSubstitutePlayers && (
+        <ul className="column is-12">
+          {benchPlayers.map((player) => (
+            <li key={player.id}>
+              <button
+                className="button is-dark is-outlined is-fullwidth test-class"
+                onClick={() => onSubstitute(player.id)}
+              >
+                {player.name + ' - ' + player.number}
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
 
       <div className="column is-12"></div>
       <div className="column is-12">
         <button
           className="button is-info is-fullwidth"
-          onClick={() => setShowSubstituteModal(true)}
+          onClick={() => setShowSubstitutePlayers(true)}
         >
           Substitute
         </button>
-        <SubstituteModal
-          team={team}
-          showModal={showSubstituteModal}
-          onClose={() => setShowSubstituteModal(false)}
-        />
       </div>
     </div>
   );
